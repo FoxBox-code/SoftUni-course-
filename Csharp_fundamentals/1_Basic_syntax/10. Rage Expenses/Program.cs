717780502bb9ae3c00f888f204170facb5aa30bd
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:41:57 +0200

    Add files via upload

---
+++ Csharp_fundamentals/1_Basic_syntax/10. Rage Expenses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostgames = int.Parse(Console.ReadLine());
+            decimal headsetprice = decimal.Parse(Console.ReadLine());
+            decimal mouseprice = decimal.Parse(Console.ReadLine());
+            decimal keyboard = decimal.Parse(Console.ReadLine());
+            decimal displayprice = decimal.Parse(Console.ReadLine());
+
+            int headsetdestyoed = lostgames / 2;
+            int mousedestroyed = lostgames / 3;
+            int keyboarddestyoed = lostgames / 6;
+            int displaydestroyed = lostgames / 12;
+
+            decimal expneses = (headsetdestyoed*headsetprice) + (mousedestroyed*mouseprice) + (keyboarddestyoed*keyboard) + (displaydestroyed * displayprice);
+            Console.WriteLine($"Rage expenses: {expneses:f2} lv.");
+
+
+        }
+    }
+}
