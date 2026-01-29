7d54697255e159a547d83a81f0b8915d0aeabf1e
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 17:06:02 +0200

    Add files via upload

---
+++ Csharp_Advanced/1_Stack_And_Queues/04. Fast Food/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int biggestOrder = int.MinValue;
+
+            int[] inLineOrrdes = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int>orders = new Queue<int>(inLineOrrdes);
+
+
+            Console.WriteLine(orders.Max());
+            while(orders.Count != 0)
+            {
+                int foodNeeded = orders.Peek();
+                
+
+                if (foodNeeded <= foodQuantity)
+                {
+                    if (biggestOrder < orders.Peek())
+                    {
+                        biggestOrder = orders.Peek();
+                    }
+                    foodQuantity -= foodNeeded;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            
+           
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ",orders)}");
+            }
+        }
+    }
+}
---
+++ Csharp_Advanced/1_Stack_And_Queues/05. Fashion Boutique/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Stack<int> clothesStack = new Stack<int>(
+                Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                );
+
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int newRack = rackCapacity;
+
+            int rackCounter = 1;
+
+            if (clothesStack.Count > 50 || rackCapacity > 20)
+            {
+                return;
+            }
+
+            while(clothesStack.Count != 0)
+            {
+                rackCapacity -= clothesStack.Peek();
+
+                if (rackCapacity < 0)
+                {
+                    rackCapacity = newRack;
+                    rackCounter++;
+                    continue;
+
+                }
+                clothesStack.Pop();
+            }
+            Console.WriteLine(rackCounter);
+
+        }
+    }
+}
