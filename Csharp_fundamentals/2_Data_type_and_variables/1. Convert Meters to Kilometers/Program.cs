4b06a85cbd9a312a3dc2a59ad76f108f57978aef
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:44:20 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/1. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1._Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+
+            double km = meters / 1000;
+
+            Console.WriteLine($"{km:f2}");
+
+
+        }
+    }
+}
