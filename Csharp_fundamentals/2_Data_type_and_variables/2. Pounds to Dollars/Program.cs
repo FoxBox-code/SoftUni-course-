8aa1e7d4a11395dc03e31ff6c5703d4023f42e47
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:44:57 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2._Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal britishpound = decimal.Parse(Console.ReadLine());
+
+            decimal dollars = britishpound * 1.31m;
+            Console.WriteLine($"{dollars:f3}");
+
+
+        }
+    }
+}
