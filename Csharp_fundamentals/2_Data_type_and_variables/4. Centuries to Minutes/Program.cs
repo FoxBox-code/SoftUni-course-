c1a10f9d1e6bdd77606f5d015d7d48e4b79f68b9
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:46:06 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/4. Centuries to Minutes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int century = int.Parse(Console.ReadLine());
+
+            int years = century * 100;
+            double days = years * 365.2422;
+            int dayss = (int)days;
+            double hours = dayss * 24;
+            double minutes = hours * 60;
+            Console.WriteLine($"{century} centuries = {years:f0} years = {dayss:f0} days = {hours:f0} hours = {minutes:f0} minutes");
+
+
+        }
+    }
+}
