0564e9348d7ace96a72fabae3b515a1d879cec21
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:38:37 +0200

    Add files via upload

---
+++ Csharp_fundamentals/7_Associative_arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string , uint> minerData = new Dictionary<string , uint>();
+
+            string command;
+
+            while((command = Console.ReadLine()) != "stop")
+            {
+                string key = command;
+                uint value = uint.Parse(Console.ReadLine());
+
+                if (!minerData.ContainsKey(key))
+                {
+                    minerData.Add(key, value);
+                }
+                else
+                {
+                    minerData[key] += value;
+                }
+            }
+
+            foreach ( var item in minerData)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Csharp_fundamentals/7_Associative_arrays/1. Count Chars in a String/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(' ');
+
+            Dictionary<char, int> lettercount = new Dictionary<char, int>();
+
+
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string currentword = strings[i];
+
+                for (int j = 0; j < currentword.Length; j++)
+                {
+                    char currentcar = currentword[j];
+                    int count = 1;
+
+                    if (!lettercount.ContainsKey(currentcar))
+                    {
+                        lettercount.Add(currentcar,count);
+                    }
+                    else
+                    {
+                        lettercount[currentcar]++;
+                    }
+                }
+
+            }
+            foreach (var letter in lettercount)
+            {
+                Console.WriteLine($"{letter.Key} -> {letter.Value}");
+            }
+        }
+    }
+}
