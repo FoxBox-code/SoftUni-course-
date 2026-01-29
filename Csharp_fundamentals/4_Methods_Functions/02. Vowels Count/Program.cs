51eca459f905d3f1b3124830ff6a8f96f4710062
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:18:25 +0200

    Add files via upload

---
+++ Csharp_fundamentals/4_Methods_Functions/02. Vowels Count/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int result = VoWSum(input);
+            Console.WriteLine(result);
+        }
+
+        static int VoWSum(string input)
+        {
+            int sumOfVowels = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = char.Parse(input[i].ToString());
+
+                if (character == 'A' || character == 'E' || character == 'O' || character == 'Y' || character == 'I' || character == 'U')
+                {
+                    sumOfVowels++;
+                }
+                if (character == 'a' || character == 'e' || character == 'o' || character == 'y' || character == 'i' || character == 'u')
+                {
+                    sumOfVowels++;
+                }
+
+            }
+            return sumOfVowels;  //when we use return. Return can only bring one value to the fold.
+        }
+    }
+}
---
+++ Csharp_fundamentals/4_Methods_Functions/1. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            SmallestNmber(n1, n2, n3);
+
+        }
+
+        private static void SmallestNmber(int n1, int n2, int n3)
+        {
+            int smallest = int.MinValue;
+            if (n1 <= n2 && n1 <= n3)
+            {
+                smallest = n1;
+            }
+            else if (n2 <= n1 && n2 <= n3)
+            {
+                smallest = n2;
+            }
+            else if (n3 <= n1 && n3 <= n2)
+            {
+                smallest = n3;
+            }
+            Console.WriteLine(smallest);
+        }
+    }
+}
