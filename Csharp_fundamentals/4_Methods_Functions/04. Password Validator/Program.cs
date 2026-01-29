4d3b5f6ea2d2b7d99bf07e9167eb249ad094bc29
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:24:13 +0200

    Add files via upload

---
+++ Csharp_fundamentals/4_Methods_Functions/04. Password Validator/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _04._Password_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenInput = Console.ReadLine();
+            int conditioncheck = 0;
+            LengthValidation(givenInput, ref conditioncheck);
+            SymbollCheck(givenInput, ref conditioncheck);
+            DigitCheck(givenInput, ref conditioncheck);
+            if (conditioncheck == 3)
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+        static void LengthValidation(string givenInput, ref int conditioncheck)
+        {
+            int letterscount = 0;
+            foreach (char c in givenInput)
+            {
+
+                char currentcharachter = c;
+                letterscount++;
+
+            }
+            if (letterscount >= 6 && letterscount <= 10)
+            {
+                conditioncheck++;
+
+            }
+            else
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+        }
+        static void SymbollCheck(string givenINput, ref int conditioncheck)
+        {
+            foreach (int i in givenINput)
+            {
+                int currentletter = i;
+                if (currentletter >= 48 && currentletter <= 57 || currentletter >= 65 && currentletter <= 90 || currentletter >= 97 && currentletter <= 122)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                    return;
+                }
+            }
+            conditioncheck++;
+        }
+        static void DigitCheck(string givenInput, ref int conditioncheck)
+        {
+            int digitcount = 0;
+            foreach (char i in givenInput)
+            {
+                int currentletter = i;
+                if (currentletter >= 48 && currentletter <= 57)
+                {
+                    digitcount++;
+                }
+
+            }
+            if (2 > digitcount)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+
+            }
+            conditioncheck++;
+        }
+    }
+}
---
+++ Csharp_fundamentals/4_Methods_Functions/04.Password Validation Altern/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _04.Password_Validation_Altern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenPassword = Console.ReadLine();
+            bool isLengthValid = IsPasswordLengthValid(givenPassword);
+            bool isAphaMerical = IsAlphaNumericValid(givenPassword);
+            bool isThereMoreThanTwoDigits = IsThereMoreThanTwoDigits(givenPassword);
+
+            if (!isLengthValid)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (!isAphaMerical)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!isThereMoreThanTwoDigits)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            if (isLengthValid && isAphaMerical && isThereMoreThanTwoDigits)
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+
+        static bool IsPasswordLengthValid(string password)
+        {
+            return password.Length >=6 && password.Length <=10;
+        }
+
+        static bool IsAlphaNumericValid(string password)
+        {
+            foreach(char ch in password)
+            {
+                if (!char.IsLetterOrDigit(ch))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool IsThereMoreThanTwoDigits(string password)
+        {
+            int digitcounter = 0;
+            foreach(char ch in password)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digitcounter++;
+                }
+            }
+            return digitcounter >= 2;
+        }
+    }
+}
