8f946bc482172b7dc040458749ba9c0fc7d68aaf
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:35:34 +0200

    Add files via upload

---
+++ Csharp_fundamentals/6_Object_and_Classes/05 Students 2.00/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Students_2_00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            List<Student> listOfSutends = new List<Student>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arrayInput = input.Split(' ');
+
+                string firstName = arrayInput[0];
+                string secondName = arrayInput[1];
+                int age = int.Parse(arrayInput[2]);
+                string town = arrayInput[3];
+
+                bool exists = false;
+
+                Student student = new Student(firstName, secondName, age, town);
+
+                foreach (var currentstudent in listOfSutends)
+                {
+                    if (currentstudent.FirstName == firstName && currentstudent.LastName == secondName)
+                    {
+                        currentstudent.Age = student.Age;
+                        currentstudent.Town = student.Town;
+                        exists = true;
+
+                    }
+                }
+                // bool exists = listOfSutends
+                                //.Any(x => x.FirstName == FirstName && x.SecondName == SecondName);
+
+                if (!exists)
+                {
+                    listOfSutends.Add(student);
+                }
+
+            }
+
+            string desiredTown = Console.ReadLine();
+
+            foreach (Student theStudent in listOfSutends)
+            {
+                if (theStudent.Town == desiredTown)
+                {
+                    Console.WriteLine($"{theStudent.FirstName} {theStudent.LastName} is {theStudent.Age} years old.");
+                }
+            }
+        }
+
+
+
+
+        public class Student
+        {
+            public Student(string firstName, string lastName, int age, string town)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Age = age;
+                Town = town;
+            }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public int Age { get; set; }
+
+            public string Town { get; set; }
+
+
+        }
+    }
+}
---
+++ Csharp_fundamentals/6_Object_and_Classes/06. Store Boxes/Program.cs
@@ -0,0 +1,87 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    namespace _06._Store_Boxes
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string input;
+
+                List<Box> boxes = new List<Box>();
+
+
+                while ((input = Console.ReadLine()) != "end")
+                {
+
+                    string[] boxInfo =  input.Split(" ");
+
+                    string serialNumber = boxInfo[0];
+                    string itemName = boxInfo[1];
+                    int itemQty = int.Parse(boxInfo[2]);
+                    decimal itemprice = decimal.Parse(boxInfo[3]);
+
+                    Item item = new Item(itemName, itemprice);
+                    Box box = new Box(serialNumber, item , itemQty);
+                    boxes.Add(box);
+
+                }
+
+                foreach (var box in boxes.OrderByDescending(x=>x.PricePerBox))
+                {
+                    Console.WriteLine(box.SerialNumber) ;
+                    Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.Quantity}");
+                    Console.WriteLine($"-- ${box.PricePerBox:f2}");
+
+
+
+                }
+            }
+        }
+
+
+
+
+
+        public class Item
+        {
+            public Item(string name, decimal price)
+            {
+                Name = name;
+                Price = price;
+            }
+
+            public string Name { get; set; }
+
+            public decimal Price { get; set; }
+
+        }
+
+        public class Box
+        {
+            public Box(string serialNumber, Item item, decimal quantity)
+            {
+                SerialNumber = serialNumber;
+                Item = item;
+                Quantity = quantity;
+            }
+
+            public string SerialNumber { get; set; }
+
+            public Item Item { get; set; }
+
+            public decimal Quantity { get; set; }
+
+            public decimal PricePerBox
+            {
+                get
+                {
+                    return Quantity * Item.Price;
+                }
+
+            }
+
+        }
+    }
