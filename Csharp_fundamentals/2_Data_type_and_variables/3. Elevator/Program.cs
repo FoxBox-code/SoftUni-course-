a24f6c06cab13f6c4152294c9bd0d7f17a75e53e
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:50:55 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/3. Elevator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numberPeople = int.Parse(Console.ReadLine());
+            decimal capacity = int.Parse(Console.ReadLine());
+
+            decimal total = Math.Ceiling(numberPeople / capacity);
+
+
+            Console.WriteLine(total);
+        }
+    }
+}
