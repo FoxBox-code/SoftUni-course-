93c0414b200f935c13bd6808a4675163917aaf86
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:50:06 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/1. Integer Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int forth = int.Parse(Console.ReadLine());
+
+            firstnumber += second;
+            firstnumber /= third;
+            firstnumber *= forth;
+
+            Console.WriteLine(firstnumber);
+
+
+        }
+    }
+}
