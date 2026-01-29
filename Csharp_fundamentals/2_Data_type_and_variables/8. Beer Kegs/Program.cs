ec7140bb0c960c1f6bf51e4886e7b74a91aecb01
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:53:36 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/8. Beer Kegs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbKegs = int.Parse(Console.ReadLine());
+            double highestPoints = double.MinValue;
+            string leader = null; 
+
+
+            for (int i = 1; i <= numbKegs; i++)
+            {
+                string name = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double score = Math.PI * radius + height;
+                if (score > highestPoints)
+                {
+                    highestPoints = score;
+                    leader = name;
+
+                }
+
+            }
+            Console.WriteLine(leader);
+        }
+    }
+}
