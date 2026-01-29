a5c09c1538c7a025250f5bfa0da2da40d62a1493
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:44:58 +0200

    Add files via upload

---
+++ Csharp_fundamentals/8_Text_Processing/01. Valid Usernames/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+
+            StringBuilder validName = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentword = input[i];
+
+
+
+                if (currentword.Length >= 3 && currentword.Length <= 16)
+                {
+                    bool nameIsValid = true;
+                    foreach (char currentCharachter in currentword)
+                    {
+
+                        if (!(char.IsLetterOrDigit(currentCharachter) || currentCharachter == '_' || currentCharachter == '-'))
+                        {
+                            nameIsValid = false;
+                            break;
+                        }
+
+                    }
+                    if (nameIsValid)
+                    {
+                        validName.AppendLine(currentword);
+                    }
+                }
+            }
+            Console.WriteLine(validName);
+        }
+    }
+}
---
+++ Csharp_fundamentals/8_Text_Processing/02. Character Multiplier/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] twoWords = Console.ReadLine()
+                .Split(" ");
+
+
+
+            string one = twoWords[0];
+            string two = twoWords[1];
+
+            int totalSum = 0;
+            totalSum = SumOfTheWords(one, two, totalSum);
+
+            Console.WriteLine(totalSum);
+
+            // to find the shortest length we can use Math.Min (stringone , stringtwo);
+
+        }
+
+        private static int SumOfTheWords(string one, string two, int totalSum)
+        {
+            if (one.Length == two.Length)
+            {
+                for (int i = 0; i < two.Length; i++)
+                {
+                    int symbolFromOne = one[i];
+                    int symbolFromTwo = two[i];
+
+
+                    int sum = symbolFromOne * symbolFromTwo;
+                    totalSum += sum;
+                }
+            }
+            else
+            {
+                if (one.Length > two.Length)
+                {
+                    for (int i = 0; i < two.Length; i++)
+                    {
+                        int symbolFromOne = one[i];
+                        int symbolFromTwo = two[i];
+
+
+                        int sum = symbolFromOne * symbolFromTwo;
+                        totalSum += sum;
+                    }
+                    for (int i = two.Length; i < one.Length; i++)
+                    {
+                        int num = one[i];
+
+                        totalSum += num;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < one.Length; i++)
+                    {
+                        int symbolFromOne = one[i];
+                        int symbolFromTwo = two[i];
+
+
+                        int sum = symbolFromOne * symbolFromTwo;
+                        totalSum += sum;
+                    }
+                    for (int i = one.Length; i < two.Length; i++)
+                    {
+                        int num = two[i];
+
+                        totalSum += num;
+                    }
+
+                }
+
+            }
+
+            return totalSum;
+        }
+    }
+}
