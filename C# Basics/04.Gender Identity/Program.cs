7c7056e81747544c58105b69beb730cce07898e8
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 14:55:57 +0200

    Add files via upload

---
+++ C# Basics/04.Gender Identity/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _04.Gender_Identity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            
+
+            if (gender == 'm') 
+                {
+                if (age >= 16) 
+                    {
+                    Console.WriteLine("Mr.");
+                
+                }
+                else 
+                {
+                    Console.WriteLine("Master");
+                
+                }
+
+            
+            }
+            else if (gender =='f') 
+                { 
+                if (age >= 16) 
+                    {
+                    Console.WriteLine("Ms.");
+
+                
+                
+                
+                }
+                else 
+                {
+                    Console.WriteLine("Miss");
+
+
+                }
+
+            
+            
+            
+            }
+        }
+    }
+}
