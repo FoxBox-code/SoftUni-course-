245d871291879a1342c02cef8d3e50d71eff6ddf
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:36:23 +0200

    Add files via upload

---
+++ Csharp_fundamentals/1_Basic_syntax/5. Login/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+
+            
+
+            while (true)
+            {
+                string enteredpassword = Console.ReadLine();
+
+                int attempts = 0;
+
+            }
+        }
+    }
+}
