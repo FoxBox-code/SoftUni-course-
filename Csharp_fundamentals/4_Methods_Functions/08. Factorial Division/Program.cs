8c3e308ce284e09e68d1362b9d1a513423a18976
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:25:55 +0200

    Add files via upload

---
+++ Csharp_fundamentals/4_Methods_Functions/08. Factorial Division/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+
+            double result = Divison(n1, n2);
+            Console.WriteLine($"{result:F2}");
+
+
+
+
+
+
+        }
+        static double FactiroialDivisonfirstNumber(double n1)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= n1; i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+        static double FactorialDivisonSecondNumber (double n2)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= n2; i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+        static double Divison(double n1 , double n2)
+        {
+            double result = (double)FactiroialDivisonfirstNumber(n1) / FactorialDivisonSecondNumber(n2);
+            return result;
+        }
+
+    }
+}
---
+++ Csharp_fundamentals/4_Methods_Functions/09. Palindrome Integers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            while ((number = Console.ReadLine()) != "END")
+            {
+                string backwards = ReverseStringWithoutInbuiltMethod(number);
+
+                if ( number == backwards)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+            
+        }
+        private static string ReverseStringWithoutInbuiltMethod(string stringInput)
+        {
+            // Reverse using While loop  
+            string reversestring = "";
+            int length;
+
+            length = stringInput.Length-1;
+
+            while (length >= 0)
+            {
+                reversestring = reversestring + stringInput[length];
+                length--;
+            }
+            return reversestring;
+
+        }
+    }
+}
