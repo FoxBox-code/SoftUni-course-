b1f084e3f3ea62bcc562fccad7a3ff74c8a60a91
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:34:01 +0200

    Add files via upload

---
+++ Csharp_fundamentals/6_Object_and_Classes/04. Students/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            List<Student> listOfSutends = new List<Student>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arrayInput = input.Split(' ');
+
+                string firstName = arrayInput[0];
+                string secondName = arrayInput[1];
+                int age = int.Parse(arrayInput[2]);
+                string town = arrayInput[3];
+
+                Student student = new Student(firstName, secondName, age, town);
+                listOfSutends.Add(student);
+
+
+            }
+
+            string desiredTown = Console.ReadLine();
+
+            foreach (Student theStudent in listOfSutends)
+            {
+                if (theStudent.Town == desiredTown)
+                {
+                    Console.WriteLine($"{theStudent.FirstName} {theStudent.LastName} is {theStudent.Age} years old.");
+                }
+            }
+        }
+
+
+
+
+        public class Student
+        {
+            public Student(string firstName, string lastName, int age, string town) 
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                Age = age;
+                Town = town;
+            }
+
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public int Age { get; set; }
+
+            public string Town { get; set; }
+
+
+        }
+    }
+}
---
+++ Csharp_fundamentals/6_Object_and_Classes/3. Songs/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+
+            List<Songs> songs = new List<Songs>();
+
+
+            for (int i = 1; i <= numb; i++)
+            {
+                string[] strings = Console.ReadLine().Split("_");
+
+                string typelist = strings[0];
+                string name = strings[1];
+                string time = strings[2];
+
+                Songs song = new Songs(typelist, name, time);
+                songs.Add(song);
+            }
+
+            string list = Console.ReadLine();
+
+            for (int i = 0; i < songs.Count; i++)
+            {
+                Songs currentsong = songs[i];
+
+                if (list == "all")
+                {
+                    Console.WriteLine(currentsong.Name);
+
+                }
+                else if (list == currentsong.TypeList)
+                {
+                    Console.WriteLine(currentsong.Name);
+                }
+            }
+        }
+
+    }
+    internal class Songs
+    {
+        public Songs(string typeList, string name, string time)
+        {
+            TypeList = typeList;
+            Name = name;
+            Time = time;
+        }
+
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+
+    }
+}
