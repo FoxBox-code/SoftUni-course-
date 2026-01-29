cfb582e445a828b43082947708647919dee28c2f
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:00:48 +0200

    Add files via upload

---
+++ Csharp_fundamentals/3_Array/04. Array Rotation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            int rotationcount = rotations % arraynumbers.Length;//This is optional but very important it optimises the code to work better if modul devision
+                                                                //find that rotation can equal zero its very beneficial.
+
+
+            for (int i = 1; i <= rotationcount; i++)
+            {
+                int firstnumb = arraynumbers[0];
+                for (int j = 0; j < arraynumbers.Length - 1; j++)
+                {
+                    arraynumbers[j] = arraynumbers[j+1];
+                }
+                arraynumbers[arraynumbers.Length-1] = firstnumb;
+            }
+            Console.WriteLine(String.Join(" ",arraynumbers));
+
+        }
+    }
+}
---
+++ Csharp_fundamentals/3_Array/3. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            int[] firstarray = new int[n];
+            int[] secondarray = new int[n];
+
+            for (int i = 1; i <= n; i++)
+            {
+                int[] number = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                int firstnum = number[0];
+                int secondnum = number[1];
+
+                if (i % 2 != 0)
+                {
+                    firstarray[i-1] = firstnum;
+                    secondarray[i-1] = secondnum;
+                }
+                else
+                {
+                    secondarray[i-1] = firstnum;
+                    firstarray[i-1] = secondnum;
+                }
+            }
+            Console.WriteLine(String.Join(" ", firstarray));
+            Console.WriteLine(String.Join(" ", secondarray));
+
+
+            
+
+
+        }
+    }
+}
