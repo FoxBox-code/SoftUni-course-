1d052214716c5c3f115ac64a6ea96e33d9a9749f
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:45:40 +0200

    Add files via upload

---
+++ Csharp_fundamentals/8_Text_Processing/04. Caesar Cipher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder cypherMessege = new StringBuilder();
+
+
+
+
+            
+                
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    int currentSymboll = input[j];
+                    int cipher = currentSymboll + 3;
+
+                cypherMessege.Append((char)cipher);
+                }
+
+            Console.WriteLine(cypherMessege);
+            
+            
+        }
+    }
+}
---
+++ Csharp_fundamentals/8_Text_Processing/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sting = new StringBuilder();
+
+            char currentCharachter = input[0];
+            Console.Write(currentCharachter);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+               currentCharachter = input[i];
+                char previus = input[i - 1];
+
+
+                
+
+                
+
+                if (previus!= currentCharachter)
+                {
+                    Console.Write(currentCharachter);
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
