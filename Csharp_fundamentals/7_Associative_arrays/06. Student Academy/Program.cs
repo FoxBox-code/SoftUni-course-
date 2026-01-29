0e3d1b9f5f4475ca1e7a99dfdb809cc91097862a
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:41:31 +0200

    Add files via upload

---
+++ Csharp_fundamentals/7_Associative_arrays/06. Student Academy/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace _06._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string , List<double>> avrageGrade = new Dictionary<string , List<double>>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!avrageGrade.ContainsKey(student))
+                {
+                    avrageGrade.Add(student, new List<double>());
+                    avrageGrade[student].Add(grade);
+                }
+                else
+                {
+                    avrageGrade[student].Add(grade);
+
+                }
+            }
+
+            foreach( var item in avrageGrade)
+            {
+                double avrageGrades = item.Value.Average();
+                if (avrageGrades < 4.50)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"{item.Key} -> {avrageGrades:f2}");
+                }
+            }
+        }
+    }
+}
---
+++ Csharp_fundamentals/7_Associative_arrays/07. Company Users/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyIds = new Dictionary<string, List<string>>();
+
+            string command = string.Empty;
+
+            while((command = Console.ReadLine()) != "End")
+            {
+
+                string[] commandArray = command
+                    .Split("->");
+
+                string company = commandArray[0].Trim();
+                string id = commandArray[1].Trim();
+                    
+                if (!companyIds.ContainsKey(company))
+                {
+                    companyIds.Add(company, new List<string>());
+                    companyIds[company].Add(id);
+                }
+                else
+                {
+                    if (companyIds[company].Contains(id))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        companyIds[company].Add(id);
+                    }
+                }
+                
+
+                
+            }
+            foreach (var details in companyIds)
+            {
+                Console.WriteLine(details.Key);
+
+                for (int i = 0; i < details.Value.Count; i++)
+                {
+                    Console.WriteLine($"-- {details.Value[i]}");
+                }
+            }
+        }
+    }
+}
