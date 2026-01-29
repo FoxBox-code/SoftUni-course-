2821fe08520ecd52756bda98021d79a0c14f21aa
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:48:04 +0200

    Add files via upload

---
+++ Csharp_fundamentals/8_Text_Processing/08/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace _08.Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double sum = 0;
+
+
+            foreach (string line in input)
+            {
+                char firstLetter = line[0];
+                char lastLetter = line[^1];
+
+                string numbInString = line.Substring(1, 2);
+
+                double num = double.Parse(numbInString);
+
+                if (char.IsUpper(firstLetter))
+                {
+                    int positionOfLetter = firstLetter - 64;
+                    num /= positionOfLetter;
+
+                }
+                else
+                {
+                    int positionOfLetter = firstLetter - 96;
+                    num *= positionOfLetter;
+
+                }
+
+                if (char.IsUpper(lastLetter))
+                {
+                    int positionOfLetter = lastLetter - 64;
+                    num -= positionOfLetter;
+                }
+                else
+                {
+                    int positionOfLetter = lastLetter - 96;
+                    num += positionOfLetter;
+                }
+                sum += num;
+
+
+
+
+
+
+            }
+            Console.WriteLine($"{sum:f2}");
+
+        }
+    }
+}
---
+++ Csharp_fundamentals/8_Text_Processing/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "923847238931983192462832102";
+
+            Console.WriteLine(num.Length);
+
+        }
+    }
+}
