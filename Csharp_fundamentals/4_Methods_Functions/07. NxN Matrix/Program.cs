615e5c4950dad5e11630c14fd7c2b4404573144e
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:24:55 +0200

    Add files via upload

---
+++ Csharp_fundamentals/4_Methods_Functions/07. NxN Matrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            NxN(n);
+        }
+
+        private static void NxN(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Csharp_fundamentals/4_Methods_Functions/5. Add and Subtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int result = SubtractionResult(n1, n2, n3);
+            Console.WriteLine(result);
+
+
+
+        }
+        static int SumOfTwoNumbers(int n1 , int n2)
+        {
+            return n1 + n2;
+        }
+        static int SubtractionResult(int n1, int n2, int n3)
+        {
+            return SumOfTwoNumbers(n1, n2) - n3;
+        }
+    }
+}
