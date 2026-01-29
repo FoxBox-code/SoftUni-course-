3ae9b91d496a1fb92f3fed579158c86eaa20eaa9
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 14:50:18 +0200

    Add files via upload

---
+++ C# Basics/Painting 1.6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Painting_1._6
+{
+    internal class Program                                                                             
+    {
+        static void Main(string[] args)
+        {
+            double protectivenylon = 1.50;  // per m2
+            double paint = 14.50; // per liter                                                                 10  11  4  8
+            double paintthiner = 5.00; // per liter
+            double bag = 0.40; 
+            
+            double needednylon = double.Parse(Console.ReadLine());
+            double neededpaint = double.Parse(Console.ReadLine());
+            double neededpaintthiner = double.Parse(Console.ReadLine());
+            double hoursforthework = double.Parse(Console.ReadLine());
+            double precent1 = 10 / 100;
+
+            double valuenaylon = (needednylon + 2) * protectivenylon;
+            double valuepaint = (neededpaint +precent1) * paint;
+            double valuepainthiner = (neededpaintthiner * paintthiner);
+
+            double totalvaluematerials = valuenaylon + valuepaint + valuepainthiner + bag;
+            double  count = (11 +  10/100) * 14.50;
+            double sumworkers = (totalvaluematerials * 30/100) * hoursforthework;
+            double endsum = totalvaluematerials + sumworkers;
+
+            Console.WriteLine(valuepaint);
+        }
+    }
+}
+                                                                                                     
