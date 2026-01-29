b3f6f38c1313b78a24ebb8169ae5c1682c995e74
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:47:23 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/7. Concat Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _7._Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname = Console.ReadLine();
+
+            
+            string fathersname = Console.ReadLine();
+            string midpoint = Console.ReadLine();
+
+            Console.WriteLine(firstname + midpoint + fathersname);
+
+
+        }
+    }
+}
