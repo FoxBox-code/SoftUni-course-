df17be19f7e0b62649e8c4d333de9466589a1a8e
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 14:47:30 +0200

    Add files via upload

---
+++ C# Basics/Radiant 1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Radiant_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radiant = double.Parse(Console.ReadLine());
+            double degree;
+            degree = radiant * 180 / Math.PI;
+            Console.WriteLine(degree);
+        }
+    }
+}
