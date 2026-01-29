130eadcfd64b0d65686fa5191fab99f1cc89261f
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:53:13 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/7. Water Overflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numPouring = int.Parse(Console.ReadLine());
+            int maxLiters = 255;
+            int start = 0;
+
+            for (int i = 1; i <= numPouring; i++)
+            {
+                int addingwater = int.Parse(Console.ReadLine());
+                start += addingwater;
+                if (start > maxLiters)
+                {
+                    start -= addingwater;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+            Console.WriteLine(start);
+        }
+    }
+}
