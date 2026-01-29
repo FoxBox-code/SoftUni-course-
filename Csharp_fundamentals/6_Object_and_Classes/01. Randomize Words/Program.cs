c0685bf807b19681bff0c70dd7d25cc9a49a80dc
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:33:08 +0200

    Add files via upload

---
+++ Csharp_fundamentals/6_Object_and_Classes/01. Randomize Words/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Random random = new Random();
+
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                
+
+                int randomNum = random.Next(0,wordsArray.Length);
+
+                string currentWord = wordsArray[i];
+                string nextWord = wordsArray[randomNum];
+
+                wordsArray[i] = nextWord;
+                wordsArray[randomNum] = currentWord;
+
+            }
+            Console.WriteLine(String.Join(Environment.NewLine, wordsArray));
+        }
+    }
+}
