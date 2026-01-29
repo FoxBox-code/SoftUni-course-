265f215952af6e6b286c953942d4bda91ad44d42
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 14:40:23 +0200

    Add files via upload

---
+++ C# Basics/Deposit 1.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Deposit_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositsum = double.Parse(Console.ReadLine());
+            double termdeposit = double.Parse(Console.ReadLine());
+            double annualprocent = double.Parse(Console.ReadLine());
+
+            double intrest = depositsum * annualprocent/100;
+            double intrestpermonth = intrest / 12;
+
+
+
+            double sum = depositsum + termdeposit * (intrestpermonth);
+            Console.WriteLine(sum);
+        }
+    }
+}
