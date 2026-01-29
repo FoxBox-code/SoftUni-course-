a92d9b52e730182e2483715d7ead2c5da4ac1a15
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 17:14:02 +0200

    Add files via upload

---
+++ Csharp_Advanced/4_Sets_And_Dictionary/10. ForceBook/Program.cs
@@ -0,0 +1,57 @@
+﻿/*using System.Text;
+
+SortedDictionary<Dictionary<string, int>, string> forceBook = new SortedDictionary<Dictionary<string, int>, string>();
+
+
+string command;
+
+while ((command = Console.ReadLine()) != "Lumpawaroo")
+{
+    string[] input = command.Split(" ");
+
+
+    if (input.Contains("|")) // Force side to Force User
+    {
+        string side = input[0];
+        StringBuilder name = new StringBuilder();
+        for (int i = 2; i < input.Length; i++)
+        {
+            name.Append(input[i]);
+        }
+        string fullName = name.ToString();
+        if (!forceBook.ContainsValue(fullName))
+        {
+            forceBook.Add(fullName, new Dictionary<string, int>());
+        }
+        if (!forceBook[fullName].ContainsKey(side))
+        {
+            forceBook[fullName].Add(side, 0);
+
+        }
+        forceBook[fullName][side]++;
+    }
+    else if (input.Contains("->")) //ForceUser to ForceSide
+    {
+        string side = input[input.Length - 1];
+        StringBuilder name = new StringBuilder();
+
+        for (int i = 0; i < input.Length - 2; i++)
+        {
+            name.Append(input[i] + " ");
+        }
+        string fullName = name.ToString();
+
+        if (!forceBook.ContainsKey(fullName))
+        {
+            forceBook.Add(fullName, new Dictionary<string, int>());
+            forceBook[fullName].Add(side, 0);
+            forceBook[fullName][side]++;
+        }
+        else
+        {
+            string currentSide = forceBook[fullName].Keys;
+
+        }
+
+    }
+}*/
---
+++ Csharp_Advanced/4_Sets_And_Dictionary/10. Solution/Program.cs
@@ -0,0 +1,76 @@
+﻿SortedDictionary<string, SortedSet<string>> sidesUsers = new SortedDictionary<string, SortedSet<string>>();
+
+
+
+string command;
+
+while ((command = Console.ReadLine()) != "Lumpawaroo")
+{
+    
+
+    if (command.Contains("|"))
+    {
+        string[] argumment = command.Split(" | ",StringSplitOptions.RemoveEmptyEntries);
+        string side = argumment[0];
+        string user = argumment[1];
+
+        if (!sidesUsers.Values.Any(n => n.Contains(user)))
+        {
+
+
+            if (!sidesUsers.ContainsKey(side))
+            {
+                sidesUsers.Add(side, new SortedSet<string>());
+
+            }
+
+            sidesUsers[side].Add(user);
+        }
+    }
+    else
+    {
+        string[] argumment = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+        string side = argumment[1];
+        string user = argumment[0]; 
+
+        foreach (var sideUser in sidesUsers)
+        {
+            if (sideUser.Value.Contains(user))
+            {
+                sideUser.Value.Remove(user);
+                break;
+            }
+        }
+        if (!sidesUsers.ContainsKey(side))
+        {
+            sidesUsers.Add(side, new SortedSet<string>());
+
+        }
+
+        sidesUsers[side].Add(user);
+
+        Console.WriteLine($"{user} joins the {side} side!");
+
+
+        
+    }
+
+    
+}
+var orderByDecending = sidesUsers
+        .OrderByDescending(x => x.Value.Count)
+        .ToDictionary(s => s.Key, s => s.Value);
+
+foreach (var order in orderByDecending)
+{
+    if (order.Value.Count > 0)
+    {
+        Console.WriteLine($"Side: {order.Key}, Members: {order.Value.Count}");
+
+        foreach (var name in order.Value)
+        {
+            Console.WriteLine($"! {name}");
+        }
+    }
+}
+
