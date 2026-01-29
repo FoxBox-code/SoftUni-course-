eac49012e47c359224296fcdc419ed8fb7eb3d10
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:01:35 +0200

    Add files via upload

---
+++ Csharp_fundamentals/3_Array/5. Top Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+           
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentnumber = array[i];
+                bool bigNumber = true;
+
+                for (int j = i +1; j < array.Length; j++)
+                {
+                    int nextnum = array[j];
+
+                    if (nextnum >= currentnumber)
+                    {
+                        bigNumber = false;
+                        break;
+                    }
+
+                }
+                if (bigNumber)
+                {
+                    Console.Write($"{currentnumber} ");
+                }
+            }
+        }
+    }
+}
