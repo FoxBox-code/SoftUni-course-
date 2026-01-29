93782cf454a22209dd4169961b21a2baad656526
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:45:34 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numnumbers = int.Parse(Console.ReadLine());
+            decimal totalsum = 0;
+
+            for (int i = 1; i <= numnumbers; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                totalsum += number;
+
+            }
+            Console.WriteLine(totalsum);
+        }
+    }
+}
