dfd44400e711c2c151d1fc6e2cb96fffee5af3ed
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:59:00 +0200

    Add files via upload

---
+++ Csharp_fundamentals/3_Array/01.Trains/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.Trains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbWagons = int.Parse(Console.ReadLine());
+
+            int[] wagon = new int[numbWagons];
+
+            int totalPasengers = 0;
+
+            for (int i = 0; i < wagon.Length; i++)
+            {
+                wagon[i] = int.Parse(Console.ReadLine());
+                totalPasengers += wagon[i];
+                Console.Write(wagon[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(totalPasengers);
+        }
+    }
+}
---
+++ Csharp_fundamentals/3_Array/2. Common Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayone = Console.ReadLine()
+                .Split()
+                .ToArray();
+            string[] arraytwo = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            for (int i = 0; i < arraytwo.Length; i++)
+            {
+                for (int j = 0; j < arrayone.Length; j++)
+                {
+                    if (arraytwo[i] == arrayone[j])
+                    {
+                        Console.Write(arrayone[j] + " ");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
