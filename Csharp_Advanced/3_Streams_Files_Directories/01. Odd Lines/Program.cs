aa8d31811c7ea0e7e529235d62e5869a82d993e0
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 17:17:50 +0200

    Add files via upload

---
+++ Csharp_Advanced/3_Streams_Files_Directories/01. Odd Lines/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Resources;
+
+namespace _01._Odd_Lines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using StreamReader sr = new StreamReader("../../../text.txt") ;
+
+            string line = sr.ReadLine() ;
+            int row = 1;
+
+            while (line != null)
+            {
+                Console.WriteLine($"{row}.{line}") ;
+                row++;
+                line = sr.ReadLine() ;
+            }
+        }
+    }
+}
