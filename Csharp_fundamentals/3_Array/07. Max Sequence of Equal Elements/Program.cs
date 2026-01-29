704c845a678a746a4e096c727e6953775448bed1
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:02:14 +0200

    Add files via upload

---
+++ Csharp_fundamentals/3_Array/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int equalcount = 1;
+            int matchedNumb = 0;
+            int maxcount = 0;
+            int definitiveNum = 0;
+            for (int i = 0; i < arrayNumbers.Length-1; i++)
+            {
+                
+                if (arrayNumbers[i] != arrayNumbers[i + 1])
+                {
+                    equalcount = 1;
+                    continue; //Numbers are not equal we go to the next index.
+                    
+                }
+                if (arrayNumbers[i] == arrayNumbers[i + 1])
+                {
+                    matchedNumb = arrayNumbers[i];
+                    equalcount++;
+                }
+                if (equalcount >= maxcount)
+                {
+                    maxcount = equalcount;
+                    definitiveNum = matchedNumb;
+                }
+            }
+            for (int i = 1; i <= maxcount; i++)
+            {
+                Console.Write(definitiveNum + " ");
+            }
+        }
+    }
+}
---
+++ Csharp_fundamentals/3_Array/8. Magic Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynumb = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int numberToEqual = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arraynumb.Length-1; i++)
+            {
+                for (int j = 1+i; j < arraynumb.Length ; j++)
+                {
+                    int sum = arraynumb[i] + arraynumb[j];
+
+                    if (sum == numberToEqual)
+                    {
+                        Console.WriteLine($"{arraynumb[i]} {arraynumb[j]}");
+
+                    }
+                }
+            }
+        }
+    }
+}
