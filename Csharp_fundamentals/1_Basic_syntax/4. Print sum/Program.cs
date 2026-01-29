aa745893ca2d25a8d81f1a03611d446e23c21dc2
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:33:38 +0200

    Add files via upload

---
+++ Csharp_fundamentals/1_Basic_syntax/4. Print sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4._Print_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int statnubmer = int.Parse(Console.ReadLine());
+            int finishnumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = statnubmer; i <= finishnumber; i++)
+            {
+                Console.Write(i + " ");
+                sum+=i;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Sum: " + sum );
+        }
+    }
+}
