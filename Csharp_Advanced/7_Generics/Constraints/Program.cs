11f027db2c18fd086e4978e08b81a6d460e7ff94
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 18:08:50 +0200

    Add files via upload

---
+++ Csharp_Advanced/7_Generics/Constraints/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+using System;
+using System.Collections;
+
+Console.WriteLine(GetBiggestElement(11,22,22));
+
+int big = GetBiggestElement(11, 22, 33);
+
+Console.WriteLine(big);
+
+
+
+T GetBiggestElement<T>(T first, T second, T third) where T : IComparable<T>
+{
+    if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+    {
+        return first;
+    }
+    if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+    {
+        return second;
+    }
+    if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+    {
+        return third;
+    }
+
+    return default(T);
+
+}
---
+++ Csharp_Advanced/7_Generics/FindGenerics/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] ints = new int[5] {1, 2 ,3 ,4, 5};
+double[] doubles = new double[5] {1.5, 2.7 ,3.3 ,4.99, 5};
+
+
+bool result = FindElement(ints, 5);
+bool results = FindElement(doubles, 0.55);
+
+Console.WriteLine(result);
+Console.WriteLine(results);
+
+
+bool FindElement<T>(T[] array , T element)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Equals(element))   // To compare data when using T generics we need to use a limited amount of given Methods in this case we use Equals
+        {
+            return true;
+        }
+    }
+    return false;
+}
+//since T cannot be sure if the two given datas are matched we can only use 4 methods.
+//defualt(T) gives us null if the data type is string or 0 if the data type is using numbers.
