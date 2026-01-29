e4cf707a15d21b7296863e539c0af5b9dbd2ac00
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 18:47:54 +0200

    Add files via upload

---
+++ OOP/2_Encapsulation/Pizza Calories/Ingridience/Dough.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_Calories.Ingridience
+{
+    public class Dough
+    {
+		private string flourType;
+		private string bakingTechnique;
+		private double weight;
+		private double modifierForFlour;
+		private double modiferForBacking;
+
+		
+
+
+		public Dough(string flourType, string backingTechnique, double weight)
+		{
+			FlourType = flourType.ToLower();
+			BakingTechnique = backingTechnique.ToLower();
+			Weight = weight;
+
+            switch (FlourType)
+            {
+                case "white":
+                    modifierForFlour = 1.5;
+                    break;
+                case "wholegrain":
+                    modifierForFlour = 1.0;
+                    break;
+
+
+            }
+            switch (BakingTechnique)
+            {
+
+                case "crispy":
+                    modiferForBacking = 0.9;
+                    break;
+                case "chewy":
+                    modiferForBacking = 1.1;
+                    break;
+                case "homemade":
+                    modiferForBacking = 1.0;
+                    break;
+
+            }
+            
+        }
+        public double ModifierForBacking
+        {
+            get { return modiferForBacking; }
+            private set
+            {
+
+                modifierForFlour = value;
+            }
+        }
+
+
+
+        public double Modifier
+		{
+			get { return modifierForFlour; }
+			private set {
+				
+				modifierForFlour = value; }
+		}
+
+
+		public double Weight
+		{
+			get { return weight; }
+            private set { 
+				if(value <1 || value > 200)
+				{
+					throw new ArgumentException("Dough weight should be in the range [1..200].");
+				}
+				weight = value; }
+		}
+
+
+		public string BakingTechnique
+		{
+			get { return bakingTechnique; }
+            private set
+            {
+				if(value != "crispy" && value != "chewy" && value != "homemade")
+				{
+					throw new ArgumentException("Invalid type of dough.");
+				}
+				bakingTechnique = value; }
+		}
+
+
+		public string FlourType
+		{
+			get { return flourType; }
+            private set
+            { 
+				if(value != "white" && value != "wholegrain")
+				{
+					throw new ArgumentException("Invalid type of dough.");
+				}
+				flourType = value; }
+		}
+		public double Calories()
+		{
+            double calories = (2 * Weight) * modifierForFlour * modiferForBacking;
+			return calories;
+        }
+		public override string ToString()
+		{
+			double calories = (2 * Weight) * modifierForFlour * modiferForBacking;
+			return calories.ToString();
+		}
+	}
+}
---
+++ OOP/2_Encapsulation/Pizza Calories/Ingridience/Topping.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_Calories.Ingridience
+{
+    public class Topping
+    {
+		private string toppingsType;
+		private double calorieModifier;
+		private double grams;
+
+		
+
+
+
+		public Topping(string toppingsType,double grams)
+		{
+			ToppingsType = toppingsType;
+			Grams = grams;
+
+			switch (ToppingsType.ToLower())
+			{
+				case "meat": calorieModifier = 1.2;
+					break;
+				case "veggies": calorieModifier = 0.8;
+					break ;
+				case "cheese": calorieModifier = 1.1;
+					break;
+				case "sauce": calorieModifier = 0.9;
+					break;
+
+            }
+		}
+        public double Grams
+        {
+            get { return grams; }
+            set {
+				
+				if(value < 0 || value > 50)
+				{
+					throw new ArgumentException($"{value} weight should be in the range [1..50].");
+				}
+				grams = value; }
+        }
+
+
+        public double CalorieModifier
+		{
+			get { return calorieModifier; }
+			set { 
+				
+				calorieModifier = value; }
+		}
+
+		public string ToppingsType
+		{
+			get { return toppingsType; }
+			set {
+				string toLower = value.ToLower();
+				if (toLower != "meat" && toLower != "veggies" && toLower != "cheese" && toLower != "sauce")
+				{
+					throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+				}
+				toppingsType = value; }
+		}
+		public double Calories()
+		{
+            double calories = (2 * grams) * calorieModifier;
+			return calories;
+        }
+
+		public override string ToString()
+		{
+			double calories = (2*grams) * calorieModifier;
+			return calories.ToString();
+		}
+	}
+}
