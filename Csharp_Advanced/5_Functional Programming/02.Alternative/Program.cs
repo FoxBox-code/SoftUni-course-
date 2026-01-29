3ed440ec050948d7a657758e326e329aea05de2a
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 17:25:47 +0200

    Add files via upload

---
+++ Csharp_Advanced/5_Functional Programming/02.Alternative/Program.cs
@@ -0,0 +1,13 @@
+﻿Action<string[], string> print = (strings, title) =>
+{
+    foreach (string s in strings)
+    {
+        Console.WriteLine($"{title} {s}");
+    }
+};
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+print(names, "Sir");
+ 
---
+++ Csharp_Advanced/5_Functional Programming/03. Custom Min Function/Program.cs
@@ -0,0 +1,21 @@
+﻿Func<int[], int> findSmallest = numbers =>
+{
+    int smallest = int.MaxValue;
+
+    foreach (var number in numbers)
+    {
+        if (number < smallest)
+        {
+            smallest = number;
+        }
+    }
+    return smallest;
+};
+
+
+int[] nums = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine(findSmallest(nums));
