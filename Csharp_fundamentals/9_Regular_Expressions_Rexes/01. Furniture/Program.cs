a6ee53381a78a00cc449c047890304ad78ed08e3
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:50:21 +0200

    Add files via upload

---
+++ Csharp_fundamentals/9_Regular_Expressions_Rexes/01. Furniture/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string demonNamePattern = @"[A-Za-z]";
+            string damage = @"([\-\+\*\/])?\d(\.\d)?";
+            string demon = "Azazel";
+
+
+
+            int healthpoints = 0;
+            double damageNum = 0;
+            int multiplycount = 0;
+            int divisoncount = 0;
+
+            foreach (char ch in demon)
+            {
+                if (ch == '*')
+                {
+                    multiplycount++;
+                }
+                else if (ch == '/');
+            }
+
+            Regex demonregex = new Regex(demonNamePattern);
+            Regex damagenums = new Regex(damage);
+
+            MatchCollection namemach = demonregex.Matches(demon);
+            MatchCollection lethality = damagenums.Matches(demon);
+
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            healthpoints = HP(healthpoints, namemach, nameBuilder);
+
+            string dName = nameBuilder.ToString();
+
+            foreach (Match m in lethality)
+            {
+                double num = double.Parse(m.Value);
+
+                damageNum += num;
+            }
+
+            if(multiplycount > 0)
+            {
+                for (int i = 1; i <= multiplycount; i++)
+                {
+                    damageNum *= 2;
+                }
+            }
+
+            Console.WriteLine(dName);
+            Console.WriteLine(healthpoints);
+            Console.WriteLine(damageNum);
+
+
+
+
+        }
+
+        private static int HP(int healthpoints, MatchCollection namemach, StringBuilder nameBuilder)
+        {
+            foreach (Match match in namemach)
+            {
+                char thiss = char.Parse(match.Value);
+                int currnum = (int)thiss;
+
+                healthpoints += currnum;
+
+                nameBuilder.Append(match.Value);
+
+            }
+
+            return healthpoints;
+        }
+    }
+}
---
+++ Csharp_fundamentals/9_Regular_Expressions_Rexes/02.RaceRedux/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02.RaceRedux
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string , int > athletes = new Dictionary<string, int>();
+
+
+            string[] runnersArray = Console.ReadLine()
+                .Split(", ");
+
+
+            foreach (string runner in runnersArray)
+            {
+                if (!athletes.ContainsKey(runner))
+                {
+                    athletes[runner] = 0;
+                }
+            }
+            string command = string.Empty;
+
+            while((command = Console.ReadLine()) != "end of race")
+            {
+                string namePattern = @"[A-Za-z]";
+                string kmPattern = @"\d";
+
+                int distancerun = 0;
+                
+
+                StringBuilder nameBuilder = new StringBuilder();
+                
+
+                Regex nameregex = new Regex(namePattern);
+                Regex kmreggex = new Regex(kmPattern);
+
+                MatchCollection namematch = nameregex.Matches(command);
+
+                if (namematch.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (Match match in namematch)
+                {
+                    nameBuilder.Append(match.Value);
+                }
+                string currentContender = nameBuilder.ToString();
+
+                if (athletes.ContainsKey(currentContender))
+                {
+                    MatchCollection distance = kmreggex.Matches(command);
+
+                    foreach (Match km in distance)
+                    {
+                        int num = int.Parse(km.Value);
+                        distancerun += num;
+
+                    }
+                    athletes[currentContender] += distancerun;
+                }
+                
+
+
+            }
+            athletes = athletes
+                    .OrderByDescending(x => x.Value)
+                    .ToDictionary(x => x.Key, y => y.Value);
+
+
+            Console.WriteLine($"1st place: {athletes.Keys.ElementAt(0)}");
+            Console.WriteLine($"2nd place: {athletes.Keys.ElementAt(1)}");
+            Console.WriteLine($"3rd place: {athletes.Keys.ElementAt(2)}");
+
+
+
+            //dict = dict
+            //.OrderByDescending(x => x.Value)
+            //.ToDictionary(x => x.Key, y => y.Value);
+
+
+
+
+        }
+    }
+}
