1ceb525a89f645f5d225f2b084c3ee11a41aae79
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:54:28 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/11.SnowBalls/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace _11.SnowBalls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte numberOfBalls = sbyte.Parse(Console.ReadLine());
+            BigInteger bestQualityball = BigInteger.MinusOne;
+            int snow = 0;
+            int time = 0;
+            int quality = 0;
+
+            for (int i = 1; i <= numberOfBalls; i++)
+            {
+                int snowBallSnow = int.Parse(Console.ReadLine());
+                int snowBallTime = int.Parse(Console.ReadLine());
+                int snowBallQuality = int.Parse(Console.ReadLine());
+
+                BigInteger score = BigInteger.Pow((snowBallSnow / snowBallTime),snowBallQuality);
+                if (score > bestQualityball)
+                {
+                    bestQualityball = score;
+                    snow = snowBallSnow;
+                    time = snowBallTime;
+                    quality = snowBallQuality;
+                }
+
+            }
+            Console.WriteLine($"{snow} : {time} = {bestQualityball} ({quality})");
+        }
+    }
+}
