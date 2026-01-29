26808502cb5c6b81c2c5d48884dc303fe077a5d3
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 17:27:11 +0200

    Add files via upload

---
+++ Csharp_Advanced/5_Functional Programming/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,42 @@
+﻿List<int> nums = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+
+
+string command;
+
+while ((command = Console.ReadLine()) != "end")
+{
+    Calculate(nums, command);
+}
+
+static void Calculate(List<int> nums, string command)
+{
+    if (command == "add")
+    {
+        for (int i = 0; i < nums.Count; i++)
+        {
+            nums[i] += 1;
+        }
+    }
+    else if (command == "multiply")
+    {
+        for (int i = 0; i < nums.Count; i++)
+        {
+            nums[i] *= 2;
+        }
+    }
+    else if (command == "subtract")
+    {
+        for (int i = 0; i < nums.Count; i++)
+        {
+            nums[i] -= 1;
+        }
+    }
+    else if (command == "print")
+    {
+        Console.WriteLine(string.Join(" ", nums));
+    }
+}
