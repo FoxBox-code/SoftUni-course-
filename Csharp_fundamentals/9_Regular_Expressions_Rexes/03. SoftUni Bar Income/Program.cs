bb7358d1aa2bcfb9cc0b6093c5a771726bbe20d1
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 16:51:11 +0200

    Add files via upload

---
+++ Csharp_fundamentals/9_Regular_Expressions_Rexes/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^[^\%\|\$\.]*?\%(?<customer>[A-Z][a-z]+)\%[^\%\|\$\.]*?\<(?<product>\w+)\>[^\%\|\$\.]*?\|(?<count>\d+)\|[^\%\|\$\.]*?(?<price>\d+(\.\d+)?)\$[^\%\|\$\.]*$";
+
+            string command;
+
+            decimal totalincome = 0;
+
+            Regex regex = new Regex(pattern);
+
+            while ((command = Console.ReadLine()) != "end of shift")
+            {
+                Match match = regex.Match(command);
+
+                if (match.Success)
+                {
+                    string customer = match.Groups["customer"].Value;
+                    string product = match.Groups["product"].Value;
+                    int count = int.Parse(match.Groups["count"].Value);
+                    decimal price = decimal.Parse(match.Groups["price"].Value);
+
+                    decimal totalprice = price * count;
+
+                    totalincome += totalprice;
+
+                    Console.WriteLine($"{customer}: {product} - {totalprice:f2}");
+                }
+
+            }
+            Console.WriteLine($"Total income: {totalincome:f2}");
+        }
+    }
+}
---
+++ Csharp_fundamentals/9_Regular_Expressions_Rexes/04.Star Enigma/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _04.Star_Enigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nMesseges = int.Parse(Console.ReadLine());
+
+            string pattern = @"^[^\@\-\!\:\>]*?\@(?<planet>[A-Za-z]+)[^\@\-\!\:\>]*?\:(?<population>\d+)[^\@\-\!\:\>]*?\!(?<attacktipe>A|D)\![^\@\-\!\:\>]*?\-\>(?<soldiercount>\d+)[^\@\-\!\:\>]*?$";
+
+            Regex regex = new Regex(pattern);
+
+            List<string> destroyedPlanets = new List<string>();
+            List<string> attackedPlanets = new List<string>();
+
+
+            for (int i = 1; i <= nMesseges; i++)
+            {
+                string decriptedMessege = Console.ReadLine();
+
+                string messege = MessegeDecription(decriptedMessege);
+
+
+                Match match = regex.Match(messege);
+
+                if (match.Success)
+                {
+                    string planet = match.Groups["planet"].Value;
+                    string attackType = match.Groups["attacktipe"].Value;
+
+                    if (attackType == "D")
+                    {
+                        destroyedPlanets.Add(planet);
+                    }
+                    else
+                    {
+                        attackedPlanets.Add(planet);
+                    }
+                }
+            }
+
+            PlanetStatus(destroyedPlanets, attackedPlanets);
+
+        }
+
+        private static void PlanetStatus(List<string> destroyedPlanets, List<string> attackedPlanets)
+        {
+            Console.WriteLine($"Attacked planets: {attackedPlanets.Count}");
+
+            foreach (string planet in attackedPlanets.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count}");
+
+            foreach (string planet in destroyedPlanets.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+        }
+
+        static string MessegeDecription(string decriptedMessege)
+        {
+            StringBuilder encriptedmessege = new StringBuilder();
+            int symbollDown = 0;
+
+            foreach(char ch in decriptedMessege.ToLower())
+            {
+                if (ch == 't' || ch == 's' || ch == 'a' || ch == 'r')
+                {
+                    symbollDown++;
+                }
+            }
+
+            foreach (char ch in decriptedMessege)
+            {
+                int decriptedSymboll = (int)ch - symbollDown;
+                encriptedmessege.Append((char)decriptedSymboll);
+
+            }
+
+            return encriptedmessege.ToString();
+
+
+        }
+    }
+}
