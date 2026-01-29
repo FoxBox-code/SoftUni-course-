3cdd93cef63c19f136982d3a06790e0cf463a6de
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 18:00:39 +0200

    Add files via upload

---
+++ Csharp_Advanced/6_Class/LinkedListAsQueAndStack/Program.cs
@@ -0,0 +1,34 @@
+﻿LinkedList<int> stack = new LinkedList<int> ();
+
+stack.AddFirst (1);
+stack.AddFirst (2);
+stack.AddFirst (3);
+stack.AddFirst (4);
+
+
+
+while(stack.Count > 0)
+{
+    int value = stack.First.Value;
+    stack.RemoveFirst();
+    Console.WriteLine(value);
+}
+//Works like a stack
+//
+
+LinkedList<int> que = new LinkedList<int>();
+
+stack.AddLast(1);
+stack.AddLast(2);
+stack.AddLast(3);
+stack.AddLast(4);
+
+
+
+while (stack.Count > 0)
+{
+    int value = stack.Last.Value;
+    stack.RemoveFirst();
+    Console.WriteLine(value);
+}
+//Works like a queue
