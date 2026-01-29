745d1d19e8a9b81db90e65643186eb06cbe205aa
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:52:18 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/5. Print Part of the ASCII Tabl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5._Print_Part_of_the_ASCII_Tabl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startnumb = int.Parse(Console.ReadLine());
+            int endnumb = int.Parse(Console.ReadLine());
+            for (int i = startnumb; i <= endnumb; i++)
+            {
+                char thiss = (char)i;
+                Console.Write(thiss+ " ");
+            }
+            
+        }
+    }
+}
