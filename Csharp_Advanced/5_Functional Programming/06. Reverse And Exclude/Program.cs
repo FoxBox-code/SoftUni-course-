0d25cdc1a13aeb23b8331e52f1db1760a3b8e79e
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 17:27:40 +0200

    Add files via upload

---
+++ Csharp_Advanced/5_Functional Programming/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int[] nums = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    
+   
+    .ToArray();
+
+int division = int.Parse(Console.ReadLine());
+
+
+
+for (int i = nums.Length-1; i >= 0; i--)
+{
+    if (nums[i] % division == 0)
+    {
+
+    }
+    else
+    {
+        Console.Write(nums[i] + " ");
+    }
+}
+
