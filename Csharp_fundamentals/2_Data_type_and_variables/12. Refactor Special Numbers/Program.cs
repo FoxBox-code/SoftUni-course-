55c3c88f191a752262ce2230702e547d8a7fcc2c
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:55:27 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int currentnumber = 0;
+
+            bool toe = false;
+
+            for (int ch = 1; ch <= num; ch++)
+
+            {
+
+                currentnumber = ch;
+
+                while (ch > 0)
+
+                {
+
+                    sum += ch % 10;
+
+                    ch = ch / 10;
+
+                }
+
+                toe = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", currentnumber, toe);
+
+                sum = 0;
+
+                ch = currentnumber;
+
+            }
+        }
+    }
+}
