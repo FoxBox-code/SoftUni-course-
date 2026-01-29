254ca6333cbae4c94b9194ced7c6ebfc4ef122fd
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:50:30 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/2. Sum Digits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            for (int i = number.Length; i > 0; i--)
+            {
+                int totalsum = 0;
+                Console.WriteLine(i);
+                totalsum += i;
+            }
+        }
+    }
+}
