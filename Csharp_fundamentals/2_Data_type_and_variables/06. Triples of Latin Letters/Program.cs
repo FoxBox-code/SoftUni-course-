824da1f4d4e56e7c7325469c1e6144a7d68b27c6
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:52:47 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/06. Triples of Latin Letters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < numb; i++)
+            {
+                for (int j = 0; j < numb; j++)
+                {
+                    for (int k = 0; k < numb; k++)
+                    {
+                        char firstchar = (char)('a' + i);
+                        char secondchar = (char)('a' + j);
+                        char thirdchar = (char)('a' + k);
+                        Console.WriteLine($"{firstchar}{secondchar}{thirdchar}");
+                    }
+                }
+            }
+            
+        }
+    }
+}
