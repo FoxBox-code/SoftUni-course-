4a27e58b5cb2922e9ba5e3037f82fbce3a6eaf02
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 14:50:52 +0200

    Add files via upload

---
+++ C# Basics/Yard Greening 1.09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Yard_Greening_1._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Garden = double.Parse(Console.ReadLine());
+            double km2 = 7.61;
+            double discount = 0.18;
+            double Jewprice = (Garden * km2  );
+            double discountonprice = Garden * km2 * discount;
+            double totalprice = Garden * km2 - discountonprice;
+            Console.WriteLine(totalprice + " lv.");
+            Console.WriteLine(discountonprice + " lv.");
+        }
+    }
+}
