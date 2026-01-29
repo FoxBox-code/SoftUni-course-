7d998e2023a1215cfa4dcff6c22c5c51130d0597
FoxBox-code <aarontodorov123619@gmail.com>
2026-01-29 15:51:20 +0200

    Add files via upload

---
+++ Csharp_fundamentals/2_Data_type_and_variables/4. Sum of Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCharacters = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+
+            for (int i = 1; i <= numCharacters; i++)
+            {
+                char input = char.Parse(Console.ReadLine());
+                totalSum += input;
+
+            }
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
